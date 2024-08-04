20c0fe6c6b85bc47a8f546d3b9f11425b16a7bb5
Alexandr Gritzuk <lexandr.gritzuk@yandex.com>
2024-08-04 07:10:47 +0300

    UPD-1.0.1-fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import App from './App.svelte';
 import './styles/style.sass';
+import './styles/sidebar.sass';
+import './styles/table.sass';
+import './styles/search.sass';
 import './styles/mobile-style.sass';
 
 const app = new App({
